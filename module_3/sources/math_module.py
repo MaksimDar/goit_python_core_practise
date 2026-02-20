3a7c8b68f773f61ab8bbcad8b61df93cbc1668a2
MaksimDar <dovhusha.m@gmail.com>
2026-02-20 17:29:51 +0100

    changes

---
+++ module_3/sources/math_module.py
@@ -0,0 +1,13 @@
+import math
+
+print(math.pow(4,4))
+
+print(math.sqrt(1024))
+print(math.floor(4.6))
+print(math.ceil(4.6))
+
+print(math.radians(66))
+
+print(math.degrees(1.56))
+
+
--- module_3/sources/randome_date.py
+++ module_3/sources/randome_date.py
@@ -7,8 +7,27 @@ def get_random_birthdays(n):
     oldest_date = current_date - timedelta(days=365*80)
     
     birthdays_list = []
-    
+    for i in range(n):
+        fake_year = random.randrange(oldest_date.year, current_date.year)
+        fake_month = random.randint(1,12)
+        fake_day = random.randint(1,31)
+        try:
+            fake_birthday = datetime(year = fake_year, month = fake_month, day=fake_day)
+        except ValueError:
+            continue
+        if current_date >= fake_birthday:
+            birthdays_list.append(fake_birthday.date())
+    return birthdays_list
 
 
 if __name__ == '__main__':
-    get_random_birthdays(10)
+    birthday_list = get_random_birthdays(5)
+
+    # print(random.choice(birthday_list))
+
+    # print(random.choices(birthday_list, k=10))
+
+    # print(random.sample(birthday_list, k=10))
+    data = [1,2,3,4,5]
+    random.shuffle(data)
+    print(data)
