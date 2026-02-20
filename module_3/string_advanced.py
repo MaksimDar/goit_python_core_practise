baf3984de1fc47fd70158fc06dcbae74d80a632f
MaksimDar <dovhusha.m@gmail.com>
2026-02-21 00:13:43 +0100

    Strings
    Advanced methods

---
+++ module_3/string_advanced.py
@@ -0,0 +1,59 @@
+# hello = """Hello!
+# How are you?
+# Are you okay?"""
+
+# print(hello)
+
+# one_line_text = "Textual data in Python is handled with str objects," \
+#                 " or strings. Strings are immutable sequences of Unicode" \
+#                 " code points. String literals are written in a variety " \
+#                 " of ways: single quotes, double quotes, triple quoted."
+
+
+# one_line_text = ("Textual data in Python is handled with str objects,"
+                
+#     " or strings. Strings are immutable sequences of Unicode"
+#         " code points. String literals are written in a variety "
+#             " of ways: single quotes, double quotes, triple quoted.")
+
+# print(one_line_text)
+
+
+# greeting="hello\nWorld!"
+
+# print(greeting)
+
+# greeting3="hello\tWorld!"
+
+# print(greeting3)
+
+# print("Hello my little\rsister")
+
+# print("Hello\\World")
+
+# print('It\'s a beautiful day')
+# print("He said, \"Hello\"")
+
+# s = "Hi there!"
+
+# start = 0
+# end = 7
+
+# print(s.find("her", start, end)) # 5
+# print(s.find("q")) # -1
+
+# s = 'Some words wonderful'
+
+# print(s.find("o"))
+# print(s.rfind('o'))
+
+# text = "apple,banana,cherry,blackberry"
+# result = text.split(',', 1)
+# print(result)  # Виведе: ['apple', 'banana', 'cherry']
+
+elements = ['earth', 'air', 'fire', 'water']
+result = '!  '.join(elements)
+print(result)  # Виведе: 'earth, air, fire, water'
+
+
+
