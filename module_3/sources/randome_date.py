a2b7c41f991b487b76b6803fa9f1dd77d8d07abf
MaksimDar <dovhusha.m@gmail.com>
2026-02-20 16:38:03 +0100

    Update randome_date.py

--- module_3/sources/randome_date.py
+++ module_3/sources/randome_date.py
@@ -1,3 +1,14 @@
 from datetime import datetime, timedelta
 import random 
 
+def get_random_birthdays(n):
+    "Get a list of random birthdays"
+    current_date = datetime.now()
+    oldest_date = current_date - timedelta(days=365*80)
+    
+    birthdays_list = []
+    
+
+
+if __name__ == '__main__':
+    get_random_birthdays(10)
